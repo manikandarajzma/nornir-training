74ba9224b901001712bc4283923859df6711d88d
Mani Govindarajan <mani.govindarajan@aus01plpt201.local>
2023-09-02 13:58:04 -0500

    printing values

--- collect-data.py
+++ collect-data.py
@@ -1,4 +1,10 @@
 import requests, json, re
+import psycopg2
+from datetime import datetime
+now = datetime.now()
+date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+
+
 
 apic_ip = 'mbdcapic1.multicare.org'
 apic_username = 'admin'
@@ -27,28 +33,31 @@ login_attributes = post_response_json['imdata'][0]['aaaLogin']['attributes']
 cookies = {}
 cookies['APIC-Cookie'] = login_attributes['token']
 
+''' psql connection '''
+conn = psycopg2.connect(database = "aci", 
+                        user = "postgres", 
+                        host= 'localhost',
+                        password = "postgres",
+                        port = 5433)
+cur = conn.cursor()
+
+
 def create_end_point_table(base_url, cookies):
     request_url = '/node/class/fvCEp.json'
 
     response_data = requests.get(base_url + request_url, cookies=cookies, verify = False )
 
-    structured_data = json.loads(response_data.text)
-    for data in structured_data['imdata']:
-        for ep in data['fvCEp'].items():
-            ep_dn = ep[1]['dn']   
-            print(ep_dn.split('/')[3].split('-')[1])
-            print(ep[1]['encap'])
-            print(ep[1]['ip'])
-            print(ep[1]['mac'])
-            print('-'* 50)
+    ep_data = json.loads(response_data.text)
+    ep_count = ep_data['totalCount']
+    print(ep_count)
+    # for data in ep_data['imdata']:
+    #     for ep in data['fvCEp'].items():
+    #         ep_dn = ep[1]['dn']   
+    #         print(ep_dn.split('/')[3].split('-')[1])
+    #         print(ep[1]['encap'])
+    #         print(ep[1]['ip'])
+    #         print(ep[1]['mac'])
+    #         print('-'* 50)
 
 create_end_point_table(base_url,cookies)
-    #for data in structured_data['imdata']:
-    #print(data)
-    # for ep in data['fvCEp'].items():
-        # ep_dn = ep[1]['dn']   
-        #print(ep_dn.split('/')[3].split('-')[1])
-        #print(ep[1]['encap'])
-        #print(ep[1]['ip'])
-        #print(ep[1]['mac'])
-        #print('-'* 50)
+
