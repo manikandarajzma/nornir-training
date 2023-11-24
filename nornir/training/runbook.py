5033790ee0b16c29923f488ed1e4ea96c4c7670d
Mani Govindarajan <mani.govindarajan@aus01plpt201.local>
2023-11-23 22:27:12 -0600

    working nornir-netmiko

---
+++ nornir/training/runbook.py
@@ -0,0 +1,14 @@
+from nornir import InitNornir
+from nornir_netmiko.tasks import netmiko_send_config
+from nornir_utils.plugins.functions import print_result
+
+nr = InitNornir(config_file="config.yaml")
+
+
+def random_config(task):
+    print(task.host['ntp_server'])
+    command_list = [f"ntp server {task.host['ntp_server']}", "interface loopback0", f"ip address {task.host['loopback']} 255.255.255.255"]
+    task.run(task=netmiko_send_config, config_commands= command_list)
+
+results = nr.run(task=random_config)
+print_result(results)
